658f9847669710ec6089787bba25f16b162726f4
myi2 <yimoises3@gmail.com>
2024-01-22 00:43:07 -0500

    HW (Session 2)

---
+++ Calc.py
@@ -0,0 +1,15 @@
+
+# Assingment 2
+
+#How many seconds are there in 42 minutes 42 seconds?
+x = (60*42)+42
+print(x)
+#How many miles are there in 10 kilometers? Hint: there are 1.61 kilometers in a mile.
+y = 10/1.61
+print(y)
+#If you run a 10 kilometer race in 42 minutes 42 seconds, what is your average pace (time per mile in minutes and seconds)? 
+solution = (x/60)/10
+print(solution)
+# What is your average speed in miles per hour?
+Solution2 = (x/60)/y
+print(Solution2)
